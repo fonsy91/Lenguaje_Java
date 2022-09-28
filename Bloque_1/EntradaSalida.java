50604972f75dac77999b41f1f2fc3bbfd0c7ecb3
alfonso91 <alfonso91fp@gmail.com>
2022-09-28 20:23:07 +0200

    final Entrada y salida

--- Bloque_1/EntradaSalida.java
+++ Bloque_1/EntradaSalida.java
@@ -1,11 +1,13 @@
 package Java.CursoJava.Bloque_1;
 
 import java.util.Scanner;   //paquete de util para poder coger valores por la entrada de teclado 
+import javax.swing.JOptionPane;
 
 public class EntradaSalida {
     public static void main(String[] args) {
         
         //Con system indicamos que la entrada sera por teclado 
+        
         Scanner entradaTeclado = new Scanner(System.in); 
         System.out.print("Hola ingrese su nombre: ");
         String nombre=entradaTeclado.nextLine();
@@ -14,6 +16,18 @@ public class EntradaSalida {
 
         System.out.println("Hola "+nombre+" bienvenido tu edad es "+edad);
         entradaTeclado.close();
+        
+        //Usando una ventana 
+        String Nombre_usuario = JOptionPane.showInputDialog("Introduce tu nombre porfavor");
+        String edad2 = JOptionPane.showInputDialog("Introduce edad ");
+
+        //parseamos la edad de tipos string a tipo int 
+        int edadParseada = Integer.parseInt(edad2);
+        System.out.println("El nombre es "+Nombre_usuario+" y la edad "+edadParseada);
+
+        //Usando el formato con printf 
+        double x = 100000.0;
+        System.out.printf("%1.2f",x/3);
 
     }
 }
